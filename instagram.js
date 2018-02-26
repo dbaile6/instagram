45e11bd733b3b7a1c5b67580fb228bc0588d3378
Dylan Bailey <dylanba1@gmail.com>
2018-02-26 09:38:20 -0500

    Add files via upload

--- instagram.js
+++ instagram.js
@@ -1,34 +1,34 @@
+var body = document.querySelector("body");
+var galleryDiv = document.createElement("div");
+var lightBoxDiv = document.createElement("div");
+var lightBoxImg = document.createElement("img");
+lightBoxDiv.classList.add("enlarged");
+galleryDiv.classList.add("gallery-class");
 
-imgContainer = document.querySelector('.img-container');
-
-imgArray = [
-    {
-        link: "photo1.jpeg"
-    },
-    {
-        link: "photo2.jpeg"
-    },
-    {
-        link: "photo3.jpeg"
-    },
+var gallery = [
+    {link: 'http://www.lovethispic.com/uploaded_images/35108-Pretty-Halloween-Bokeh.jpg'},
+    {link: 'http://www.our3dvr.com/data/wallpapers/64/WDF_1097626.jpg'},
+    {link: 'https://orig00.deviantart.net/160f/f/2010/118/8/5/pretty_nature_by_tylercuddlebutt.jpg'},
+    {link: 'https://wallpaper.wiki/wp-content/uploads/2017/05/wallpaper.wiki-Pretty-HD-Photo-Tumblr-PIC-WPE004194.jpg'}
 ];
+body.appendChild(lightBoxDiv);
+body.appendChild(galleryDiv);
+lightBoxDiv.appendChild(lightBoxImg);
 
-imgArray.forEach(function (picture) {
-    var imgElement = document.createElement('img');
-    imgElement.src = picture.link;
-    imgContainer.appendChild(imgElement);
-});
 
-imgElement
-
-if (typeof imgContainer !== 'undefined') {
-    /*we are good
-    */
-
-    function pictureClick() {
-        document.getElementById('imgArray').click();
-        console.log('You clicked!')
-    }
-
-    imgArray.addEventListener('click,', pictureClick);
+for (var i = 0; i<gallery.length; i++) {
+    var pic = gallery[i];
+    var galImg = document.createElement('img');
+    galImg.src = pic.link;
+    galleryDiv.appendChild(galImg);
+    galleryDiv.classList.add('gallery-class');
+    galImg.addEventListener('click', lightBox);
+}
+function lightBox() {
+    lightBoxImg.setAttribute("src", this.src);
+    lightBoxDiv.classList.toggle('visible');
+    lightBoxImg.addEventListener('click', normalState);
+}
+function normalState() {
+    lightBoxDiv.classList.toggle('visible');
 }
