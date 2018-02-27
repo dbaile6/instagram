c0d2c5c58da5f8639d37c5c505f4b3d63f713224
Dylan Bailey <dylanba1@gmail.com>
2018-02-27 14:38:09 -0500

    Rudimentary jQuery support.

--- instagram.js
+++ instagram.js
@@ -1,34 +1,49 @@
+var body = document.querySelector('.body');
+var images = ['photo1.jpeg', 'photo2.jpeg', 'photo3.jpeg', 'photo4.jpeg'];
+var images1 = []; 
 
-imgContainer = document.querySelector('.img-container');
 
-imgArray = [
-    {
-        link: "photo1.jpeg"
-    },
-    {
-        link: "photo2.jpeg"
-    },
-    {
-        link: "photo3.jpeg"
-    },
-];
-
-imgArray.forEach(function (picture) {
-    var imgElement = document.createElement('img');
-    imgElement.src = picture.link;
-    imgContainer.appendChild(imgElement);
-});
-
-imgElement
-
-if (typeof imgContainer !== 'undefined') {
-    /*we are good
-    */
-
-    function pictureClick() {
-        document.getElementById('imgArray').click();
-        console.log('You clicked!')
+//the loop that puts my images on the page
+var pictures = function (x) {
+    for (var i = 0; i < x.length; i++) {
+        var img = x[i];
+        img = document.createElement('img');
+        img.setAttribute("src", x[i]);
+        body.appendChild(img);
+        images1.push(img); //puts images in new array
     }
+};
+pictures(images); //runs previous function
+var main = document.getElementById('currentImg');
+var divM = document.querySelector('.addmain');
 
-    imgArray.addEventListener('click,', pictureClick);
+var hide = function (e) {
+    main.setAttribute("src", 'blank.png');
+    divM.classList.toggle('hide');
+    divM.classList.remove('main');
 }
+
+//define function for what will happen on mouse click
+var alert = function (e) {
+    var img = this.getAttribute("src");
+    main.setAttribute("src", img);
+    divM.classList.add('main');
+    divM.addEventListener('click', hide)
+}
+
+//use loop to add mouseclick function to each image in array
+var clickPictures = function (x) {
+    for (var i = 0; i < x.length; i++) {
+        var img = x[i];
+        img.addEventListener('click', alert);
+    }
+}
+
+//run function
+clickPictures(images1)
+
+// var close = function(e) {
+//     divM.classList.remove('.main');
+// }
+
+// main.addEventLister('click', close);
